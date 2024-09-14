1a70a402d9bb439d71f0f7c3e6b966ad378d69f0
H A S A N <hasan25042019@gmail.com>
2024-09-14 22:27:52 +0600

    dfg

--- vue_laravel/vue/src/components/category_modal.vue
+++ vue_laravel/vue/src/components/category_modal.vue
@@ -130,7 +130,7 @@
         <footer
           class="flex flex-col items-center justify-end px-6 py-3 -mx-6 -mb-4 space-y-4 sm:space-y-0 sm:space-x-6 sm:flex-row bg-gray-50 dark:bg-gray-800"
         >
-        
+    
         <div
           role="button"
           tabindex="0"
@@ -141,7 +141,6 @@
         >
           Cancel
         </div>
-
         <div
           role="button"
           tabindex="0"
